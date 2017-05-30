4470edfb848176fb46c8d020959ef471743a85cd
Borja González <bgonzalez@freepik.com>
2017-05-30 13:38:40 +0200

    Added storage library to bobjoll

---
+++ ts/library/storage.ts
@@ -0,0 +1,55 @@
+function getStorage(storageType: string): Storage | null {
+    try {
+        var storage = (window as any)[storageType + 'Storage'],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return storage;
+    }
+    catch(e) {
+        return null;
+    }
+}
+
+export class ClientStorage
+{
+    private backend: Storage | null;
+    private dummy: { [key: string]: string };
+
+    constructor(storageType: 'local' | 'session') {
+        this.backend = getStorage(storageType);
+        if (!this.backend) {
+            this.dummy = {};
+        }
+    }
+
+    supported(): boolean {
+        return !!this.backend;
+    }
+
+    getItem(namespace: string, key: string): string | null {
+        const k = namespace + '/' + key;
+        return this.backend ? this.backend.getItem(k) : this.dummy[k];
+    }
+
+    setItem(namespace: string, key: string, value: string) {
+        const k = namespace + '/' + key;
+        if (this.backend) {
+            this.backend.setItem(k, value);
+        }
+        else {
+            this.dummy[k] = value;
+        }
+    }
+
+    get(namespace: string, key: string): any | null {
+        return JSON.parse(this.getItem(namespace, key) || 'null');
+    }
+
+    set(namespace: string, key: string, value: any) {
+        this.setItem(namespace, key, JSON.stringify(value));
+    }
+}
+
+export let localStorage = new ClientStorage('local');
+export let sessionStorage = new ClientStorage('session');
