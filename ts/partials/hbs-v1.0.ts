f058d7aa82ff52671c980243220cab9707605795
Joel Kobjoll <jkobjoll@freepik.com>
2018-05-08 18:14:53 +0200

    Removed template type

--- ts/partials/hbs-v1.0.ts
+++ ts/partials/hbs-v1.0.ts
@@ -1,13 +1,9 @@
 require('BobjollTemplate/helpers.js');
 
-interface Template {
-    (data?: Object): string;
-}
-
 export default class View {
     public static ext: string = 'html.hbs';
 
-    public static render(template: Template, data?: Object) {
+    public static render(template: Function, data?: Object) {
         return template(data);
     }
 }
