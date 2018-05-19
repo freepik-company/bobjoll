ef6bd8cf69c4c4111d4752d1def6b41078919a04
Freepik <freepik@MacBook-Pro-de-Joel.local>
2018-05-19 13:13:07 +0200

    Fix modal optional preventDefault

--- ts/partials/modal-v1.0.ts
+++ ts/partials/modal-v1.0.ts
@@ -180,7 +180,8 @@ class Modal {
 
         EventListenerOn('body', '.modal__trigger', 'click', function(this: HTMLElement, e: Event) {
             if (!this.dataset.allowDefault) {
-                e.preventDefault;
+                e.preventDefault();
+                e.stopPropagation();
             }
 
             let id = this.dataset['modal'];
