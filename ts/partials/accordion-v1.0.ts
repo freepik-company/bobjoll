3ba04a273926e19443c82f9cc9e21bafd6a3e204
Joel Kobjoll <jkobjoll@freepik.com>
2019-10-04 11:28:22 +0200

    Add disable-mobile-select to accordions to disable select menu

--- ts/partials/accordion-v1.0.ts
+++ ts/partials/accordion-v1.0.ts
@@ -3,99 +3,100 @@ import { delegate, delegateRemove, qq } from 'bobjoll/ts/library/dom';
 import { IsUrlValid } from 'bobjoll/ts/library/helpers';
 
 class Accordion {
-	private static instance: Accordion | null;
+    private static instance: Accordion | null;
 
-	constructor() {
-		if (!Accordion.instance) {
-			Accordion.instance = this;
-			this.setup();
-		}
+    constructor() {
+        if (!Accordion.instance) {
+            Accordion.instance = this;
+            this.setup();
+        }
 
-		return Accordion.instance;
-	}
+        return Accordion.instance;
+    }
 
-	private static addEventListeners() {
-		delegate('.accordion__link', 'click', Accordion.eventClickHandler);
-	}
+    private static addEventListeners() {
+        delegate('.accordion__link', 'click', Accordion.eventClickHandler);
+    }
 
-	private static setupMobileNavigation() {
-		qq('.accordion:not(.accordion--ready)').forEach(element => {
-			const options = qq('.accordion__container > a, .accordion__container > button', element)
-				.reduce((acc, link, index) => {
-					link.classList.add('hide-tablet');
+    private static setupMobileNavigation() {
+        qq('.accordion:not(.accordion--ready)').forEach(element => {
+            const options = qq('.accordion__container > a, .accordion__container > button', element).reduce((acc, link, index) => {
+                if (!('' == element.dataset.disableMobileSelect)) {
+                    link.classList.add('hide-tablet');
+                }
+                acc += `<option value="${index}">${link.innerHTML}</option>`;
+                return acc;
+            }, '');
 
-					acc += `<option value="${index}">${link.innerHTML}</option>`;
+            if (!('' == element.dataset.disableMobileSelect)) {
+                element.insertAdjacentHTML(
+                    'afterbegin',
+                    `
+					<li class="accordion__mobile-nav show-tablet">
+						<div class="accordion__select__wrapper">
+							<select class="accordion__select">${options}</select>
+						</div>
+					</li>
+				`,
+                );
+            }
 
-					return acc;
-				}, '');
+            element.classList.add('accordion--ready');
+        });
 
-			element.insertAdjacentHTML('afterbegin', `
-                <li class="accordion__mobile-nav show-tablet">
-                    <div class="accordion__select__wrapper">
-                        <select class="accordion__select">${options}</select>
-                    </div>
-                </li>
-            `);
+        qq('.accordion__select').forEach(select => select.addEventListener('change', Accordion.eventSelectChangeHandler));
+    }
 
-			element.classList.add('accordion--ready');
-		});
+    private static eventClickHandler(this: HTMLElement) {
+        const wrapper = <HTMLElement | undefined>this.parent('.accordion');
 
-		qq('.accordion__select')
-			.forEach(select => select.addEventListener('change', Accordion.eventSelectChangeHandler));
-	}
+        if (wrapper) {
+            const collapsible = 'true' === (wrapper.dataset.collapsible || wrapper.dataset.closable);
 
-	private static eventClickHandler(this: HTMLElement) {
-		const wrapper = <HTMLElement | undefined>this.parent('.accordion');
+            qq('.accordion__link', wrapper).forEach(item => (item !== this ? item.classList.remove('active') : null));
 
-		if (wrapper) {
-			const collapsible = 'true' === (wrapper.dataset.collapsible || wrapper.dataset.closable);
+            (wrapper.classList as any)[collapsible ? 'toggle' : 'add']('active');
+            (this.classList as any)[collapsible ? 'toggle' : 'add']('active');
+        }
+    }
 
-			qq('.accordion__link', wrapper).forEach(item => item !== this ? item.classList.remove('active') : null);
+    private static eventSelectChangeHandler(this: HTMLSelectElement) {
+        const indexActive = parseFloat(this.value);
+        const wrapper = this.parent('.accordion');
 
-			(wrapper.classList as any)[collapsible ? 'toggle' : 'add']('active');
-			(this.classList as any)[collapsible ? 'toggle' : 'add']('active');
-		}
-	}
+        if (wrapper && 'number' === typeof indexActive) {
+            qq('.accordion__container > a, .accordion__container > button', wrapper as HTMLElement).forEach((button, index) => {
+                const href = button.getAttribute('href');
 
-	private static eventSelectChangeHandler(this: HTMLSelectElement) {
-		const indexActive = parseFloat(this.value);
-		const wrapper = this.parent('.accordion');
+                if (button.classList.contains('accordion__link')) {
+                    if (indexActive === index) {
+                        button.click();
+                    }
 
-		if (wrapper && 'number' === typeof indexActive) {
-			qq('.accordion__container > a, .accordion__container > button', (wrapper as HTMLElement))
-				.forEach((button, index) => {
-					const href = button.getAttribute('href');
+                    button.classList[indexActive === index ? 'add' : 'remove']('active');
+                } else if (href && indexActive === index && IsUrlValid(href)) {
+                    window.location.href = href;
+                }
+            });
+        }
+    }
 
-					if (button.classList.contains('accordion__link')) {
-						if (indexActive === index) {
-							button.click();
-						}
+    public refresh() {
+        Accordion.setupMobileNavigation();
+    }
 
-						button.classList[indexActive === index ? 'add' : 'remove']('active');
-					} else if (href && indexActive === index && IsUrlValid(href)) {
-						window.location.href = href;
-					}
-				});
-		}
-	}
+    public destroy() {
+        qq('.accordion__select').forEach(select => select.removeEventListener('change', Accordion.eventSelectChangeHandler));
 
-	public refresh() {
-		Accordion.setupMobileNavigation();
-	}
+        delegateRemove('.accordion__link', 'click', Accordion.addEventListeners);
 
-	public destroy() {
-		qq('.accordion__select')
-			.forEach(select => select.removeEventListener('change', Accordion.eventSelectChangeHandler));
+        Accordion.instance = null;
+    }
 
-		delegateRemove('.accordion__link', 'click', Accordion.addEventListeners);
-
-		Accordion.instance = null;
-	}
-
-	private setup() {
-		Accordion.setupMobileNavigation();
-		Accordion.addEventListeners();
-	}
+    private setup() {
+        Accordion.setupMobileNavigation();
+        Accordion.addEventListeners();
+    }
 }
 
 const accordion = new Accordion();
