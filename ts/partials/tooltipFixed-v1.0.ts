4db13716a9d730a96364ea043df6aa24bb7e8350
Bea Ramón <bramon@freepik.com>
2021-09-30 12:27:02 +0200

    FREE-2428 TooltipFixed: Button to reappear tooltip and breakpoints to show it

--- ts/partials/tooltipFixed-v1.0.ts
+++ ts/partials/tooltipFixed-v1.0.ts
@@ -7,16 +7,22 @@ export class TooltipFixed {
     private static sheet: HTMLStyleElement;
     private static wrapperElement: HTMLElement | null;
     private static tooltipElement: HTMLElement | null;
+    private static cookieFinalValue = '1';
 
     public static add(settings: TooltipFixedSettings) {
         TooltipFixed.cookieName = `tooltipFixed-${settings.cookieId}`;
         TooltipFixed.wrapperElement = q(settings.elementToInsert);
 
-        if (cookie.getItem(TooltipFixed.cookieName) || !TooltipFixed.wrapperElement) return;
+        const cookieValue = cookie.getItem(TooltipFixed.cookieName);
+
+        if ((cookieValue && cookieValue === '1') || !TooltipFixed.wrapperElement) return false;
+
+        if (cookieValue && !this.showReappearedTooltip(cookieValue)) return false;
 
         if (!settings.tooltip) {
             settings.tooltip = TooltipFixed.wrapperElement.innerHTML;
         }
+
         settings.setStyle && TooltipFixed.setStyle(settings);
 
         TooltipFixed.wrapperElement.innerHTML = this.templateContent({
@@ -30,50 +36,75 @@ export class TooltipFixed {
             ? q('.tooltip__container', TooltipFixed.wrapperElement)
             : TooltipFixed.wrapperElement;
 
+        settings.breakpoint && TooltipFixed.showTooltipFromBreakpoint(settings.breakpoint);
+
         !settings.insertContainer && TooltipFixed.wrapperElement.classList.add('tooltipFixed__active');
 
         TooltipFixed.closeEvent(settings);
+
+        return TooltipFixed.tooltipElement;
+    }
+
+    private static showTooltipFromBreakpoint(breakpoint: MaxMinBreakpoint) {
+        const { from, min } = breakpoint;
+
+        ['tooltipFixed__breakpoint', `tooltipFixed__breakpoint-${from}`, `tooltipFixed__breakpoint-${from}--${min ? 'min' : 'max'}`]
+            .forEach(classes => TooltipFixed.tooltipElement?.classList.add(classes));
+    }
+
+    private static showReappearedTooltip(cookieValue: string) {
+        if (cookieValue !== '1' && new Date() > new Date(parseInt(cookieValue))) {
+            return true;
+        }
+
+        return false;
     }
 
     private static getExpireCookie(days: number) {
         return new Date().getTime() + 24 * days * 60 * 60 * 1000;
     }
 
+    private static initClickEvent(settings: TooltipFixedSettings, cookieValue: string, cookieExpires: Date) {
+        if (cookie.getItem(TooltipFixed.cookieName)) {
+            cookieValue = this.cookieFinalValue;
+        }
+
+        cookie.setItem(
+            TooltipFixed.cookieName,
+            cookieValue, {
+            expires: cookieExpires,
+        });
+
+        if (settings.tooltip) TooltipFixed.wrapperElement!.innerHTML = settings.tooltip;
+
+        TooltipFixed.tooltipElement?.classList.remove('tooltipFixed__active');
+    }
+
     private static closeEvent(settings: TooltipFixedSettings) {
         if (!TooltipFixed.wrapperElement) return;
 
-        const cookieExpiresAt30days = TooltipFixed.getExpireCookie(30);
+        const cookieExpiresAt30days = new Date(TooltipFixed.getExpireCookie(30));
+        const cookieExpiresAt365days = new Date(TooltipFixed.getExpireCookie(365));
+        const cookieReappearValue = TooltipFixed.getExpireCookie(settings.timesToReappearInDays || 1).toString();
 
         qq('.notification__close, .button-close', TooltipFixed.wrapperElement).forEach((buttonElement) =>
             buttonElement.addEventListener('click', () => {
-                if (!settings.buttonCloseForever) {
-                    cookie.setItem(
-                        TooltipFixed.cookieName,
-                        '1', {
-                        expires: new Date(cookieExpiresAt30days),
-                    });
-                }
-
-                if (settings.tooltip) TooltipFixed.wrapperElement!.innerHTML = settings.tooltip;
-
-                TooltipFixed.tooltipElement?.classList.remove('tooltipFixed__active');
+                this.initClickEvent(
+                    settings,
+                    settings.buttonCloseForever ? cookieReappearValue : this.cookieFinalValue,
+                    settings.buttonCloseForever ? cookieExpiresAt365days : cookieExpiresAt30days
+                )
             }),
         );
 
         if (settings.buttonCloseForever) {
-            const cookieExpiresAt365days = TooltipFixed.getExpireCookie(365);
-
-            q('.button-close-forever', TooltipFixed.wrapperElement)?.addEventListener('click', () => {
-                cookie.setItem(
-                    TooltipFixed.cookieName,
-                    '1', {
-                    expires: new Date(cookieExpiresAt365days),
-                });
-
-                if (settings.tooltip) TooltipFixed.wrapperElement!.innerHTML = settings.tooltip;
-
-                TooltipFixed.tooltipElement?.classList.remove('tooltipFixed__active');
-            });
+            q('.button-close-forever', TooltipFixed.wrapperElement)?.addEventListener('click', () =>
+                this.initClickEvent(
+                    settings,
+                    this.cookieFinalValue,
+                    new Date(cookieExpiresAt365days)
+                )
+            );
         }
     }
 
@@ -97,9 +128,16 @@ export class TooltipFixed {
 
 type TooltipAppearance = 'light' | 'blue';
 type TooltipPosition = 'top' | 'bottom' | 'left' | 'right';
+type TooltipBreakpoint = 'xs' | 'sm' | 'md' | 'lg';
+
+interface MaxMinBreakpoint {
+    from: TooltipBreakpoint;
+    min: boolean;
+}
 
 export interface TooltipFixedSettings {
     appearance?: TooltipAppearance;
+    breakpoint?: MaxMinBreakpoint;
     buttonCloseForever?: boolean;
     cookieId: string;
     elementToInsert: string;
@@ -107,5 +145,6 @@ export interface TooltipFixedSettings {
     insertContainer: boolean;
     position?: TooltipPosition;
     setStyle?: { [name: string]: any };
+    timesToReappearInDays?: number;
     tooltip?: string;
 }
